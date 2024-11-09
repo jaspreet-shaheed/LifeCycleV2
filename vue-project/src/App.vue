3353553ee65c826d20bbbeda6721601c2e7ca40a
jaspreet.shaheed <jaspreet.shaheed@gmail.com>
2024-11-09 19:06:37 +0000

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import Title from './components/TitleBanner.vue'
+import Grid from './components/GameGrid.vue'
+
+</script>
+
+<template>
+  <div class="container">
+  <header>
+    <div class="wrapper">
+      <Title msg="LifeCycle Game v2" />
+    </div>
+  </header>
+  
+  <Grid :squares=9></Grid>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .container {
+    display: grid;
+    place-items: center; /* Centers both horizontally and vertically */
+  }
+}
+</style>
---
+++ vue-project/src/components/GameGrid.vue
@@ -0,0 +1,45 @@
+
+<script setup lang="ts">
+import * as _ from "lodash";
+
+const props = defineProps<{
+  squares: number
+}>()
+
+const numbers = _.range(0, (props.squares * 2) -1 , 1);
+
+
+</script>
+
+<template>
+  <table>
+    <tr v-for="yCoord in numbers" 
+      :key="yCoord">
+      <td v-for="xCoord in numbers" 
+        :key="xCoord-yCoord"
+        v-bind:class="(xCoord % 2 == 0 && yCoord % 2 == 0)?'piece':(xCoord % 2 == 0)?'yPath':(yCoord % 2 ==0)?'xPath':'nothing'  ">
+        ({{ xCoord }},{{ yCoord }})
+      </td>
+    </tr>
+  </table>
+</template>
+
+
+<style scoped>
+
+.piece {
+  border-style: solid;
+}
+
+.xPath {
+  width: 100%;
+  height: 2px;
+  background-color: black;
+}
+
+.yPath {
+}
+
+.nothing {
+}
+</style>
