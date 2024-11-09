647e614526ba2564ac7529c72f34bc4967aeae42
jaspreet.shaheed <jaspreet.shaheed@gmail.com>
2024-11-09 19:50:43 +0000

    Header centered

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,46 +7,22 @@ import Grid from './components/GameGrid.vue'
 
 <template>
   <div class="container">
-  <header>
     <div class="wrapper">
-      <Title msg="LifeCycle Game v2" />
+      <Title msg="LifeCycle: The Game" style="text-align: center" />
     </div>
-  </header>
   
   <Grid :squares=9></Grid>
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .container {
+.container {
     display: grid;
     place-items: center; /* Centers both horizontally and vertically */
   }
+
+header {
+  line-height: 1.5;
 }
 </style>
